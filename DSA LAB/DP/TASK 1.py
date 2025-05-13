19a738b3e151cd7e2b073c61d6d9b5165e4ab0f7
SeereenAyyaz <seereenayaz@gmail.com>
2025-05-13 21:47:23 +0500

    Update TASK 1.py

---
+++ DSA LAB/DP/TASK 1.py
@@ -0,0 +1,17 @@
+def fib_memoization(n, memo=None):
+    if memo is None:
+        memo = {}
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    memo[n] = fib_memoization(n - 1, memo) + fib_memoization(n - 2, memo)
+    return memo[n]
+def fib_tabulation(n):
+    if n <= 1:
+        return n
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[n]
