a3b4e2405a0c8ced7a1e7bed79bfa9104298bc6c
SeereenAyyaz <seereenayaz@gmail.com>
2025-05-13 21:55:14 +0500

    Update TASK 3.py

---
+++ DSA LAB/GD/TASK 3.py
@@ -0,0 +1,35 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x]) 
+        return self.parent[x]
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.parent[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.parent[rootX] = rootY
+            else:
+                self.parent[rootY] = rootX
+                self.rank[rootX] += 1
+
+def kruskal(edges, num_vertices):
+    edges.sort(key=lambda x: x[2])
+
+    disjoint_set = DisjointSet(num_vertices)
+    mst = []
+
+    for u, v, weight in edges:
+        if disjoint_set.find(u) != disjoint_set.find(v):
+            disjoint_set.union(u, v)
+            mst.append((u, v, weight))
+    
+    return mst
+
